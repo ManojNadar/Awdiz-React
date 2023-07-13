866e9a53f30d2a942fc35b8a4d5b790e5b3d0ea2
ManojNadar <manojviru03@gmail.com>
2023-07-13 15:11:00 +0530

    updates`

--- reactawdiz/src/components/FetchProducts/FetchSingleProduct.js
+++ reactawdiz/src/components/FetchProducts/FetchSingleProduct.js
@@ -3,30 +3,41 @@ import { useParams } from "react-router-dom";
 
 const FetchSingleProduct = () => {
   const [singleData, setSingleData] = useState([]);
-  const param = useParams();
+  const [singleProd, setSingleProd] = useState({});
+  const { id } = useParams();
 
-  //   console.log(param);
-
-  const { id } = param;
-  console.log(singleData);
+  // useEffect(() => {
+  //   fetch(`https://fakestoreapi.com/products/${id}`)
+  //     .then((res) => res.json())
+  //     .then((res) => setSingleData(res));
+  // }, []);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
+    fetch(`https://fakestoreapi.com/products/`)
       .then((res) => res.json())
       .then((res) => setSingleData(res));
   }, []);
 
+  useEffect(() => {
+    if (id && singleData.length) {
+      const findProduct = singleData.find((elem) => elem.id === parseInt(id));
+      setSingleProd(findProduct);
+    }
+  }, [id, singleData]);
+
+  // console.log(singleProd);
+
   return (
     <div id="singleProdContainer">
       <div id="singleImgContainer">
-        <img src={singleData.image} alt="" />
+        <img src={singleProd.image} alt="" />
       </div>
 
       <div id="singleProdDetails">
-        <h3>Title : {singleData.title}</h3>
-        <p>Description : {singleData.description}</p>
-        <h2>Rs.{singleData.price}</h2>
-        <i>Category : {singleData.category}</i>
+        <h3>Title : {singleProd.title}</h3>
+        <p>Description : {singleProd.description}</p>
+        <h2>Rs.{singleProd.price}</h2>
+        <i>Category : {singleProd.category}</i>
       </div>
     </div>
   );
